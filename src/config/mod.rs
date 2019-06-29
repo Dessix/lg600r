0b16d70d719d91fe9ac972745acfce2156371c19
Zoey <Dessix@Dessix.net>
2019-06-29 00:18:59 -0700

    Format pass

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -135,10 +135,19 @@ fn test_parse_config() -> () {
         115 = { type = "keyboard", key = "F1" }
     "#;
     let res = parse_config_from_toml_string(&String::from(input)).expect("Must pass");
-    assert_eq!(res.bindings[0], (9u32, BindingType::Command("wooo".to_string())));
+    assert_eq!(
+        res.bindings[0],
+        (9u32, BindingType::Command("wooo".to_string()))
+    );
     assert_eq!(res.bindings[1], (113u32, BindingType::EmulateMouse(9)));
-    assert_eq!(res.bindings[2], (115u32, BindingType::EmulateKey(::xdo::Key::F1)));
-    assert_eq!(res.bindings[3], (12u32, BindingType::Command("tadah".to_string())));
+    assert_eq!(
+        res.bindings[2],
+        (115u32, BindingType::EmulateKey(::xdo::Key::F1))
+    );
+    assert_eq!(
+        res.bindings[3],
+        (12u32, BindingType::Command("tadah".to_string()))
+    );
 }
 
 fn load_dotfile_contents(dotfilepath: &::std::path::Path) -> ::std::io::Result<String> {
