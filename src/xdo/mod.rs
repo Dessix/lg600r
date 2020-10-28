12dafb4c81e8b3f2ba92c6d2765d5614aa8c4ee7
Zoey <Dessix@Dessix.net>
2020-10-28 03:04:09 -0700

    Added support for number bindings
    This xdo thing is terrible

--- src/xdo/mod.rs
+++ src/xdo/mod.rs
@@ -125,6 +125,27 @@ pub enum Key {
     Layout(char),
     /// raw keycode eg 0x38
     Raw(u16),
+    /// number row
+    #[serde(rename = "1")]
+    Number1,
+    #[serde(rename = "2")]
+    Number2,
+    #[serde(rename = "3")]
+    Number3,
+    #[serde(rename = "4")]
+    Number4,
+    #[serde(rename = "5")]
+    Number5,
+    #[serde(rename = "6")]
+    Number6,
+    #[serde(rename = "7")]
+    Number7,
+    #[serde(rename = "8")]
+    Number8,
+    #[serde(rename = "9")]
+    Number9,
+    #[serde(rename = "0")]
+    Number0,
 }
 
 mod formatting_impls {
